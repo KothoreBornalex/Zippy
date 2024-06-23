7e170c7429c8d9714f5e456202cbb486aaaa08df
Kothore <100525076+KothoreBornalex@users.noreply.github.com>
2024-06-23 23:58:40 +0200

    Revert "cleanup"
    This reverts commit 2be0d70421339337ad59851a6e19cee011f40d66.

--- Source/Zippy/Private/ZippyCharacter.cpp
+++ Source/Zippy/Private/ZippyCharacter.cpp
@@ -82,8 +82,11 @@ void AZippyCharacter::MoveForward(float Value)
 {
 	if ((Controller != nullptr) && (Value != 0.0f))
 	{
+		// find out which way is forward
 		const FRotator Rotation = Controller->GetControlRotation();
 		const FRotator YawRotation(0, Rotation.Yaw, 0);
+
+		// get forward vector
 		const FVector Direction = FRotationMatrix(YawRotation).GetUnitAxis(EAxis::X);
 		AddMovementInput(Direction, Value);
 	}
@@ -93,9 +96,13 @@ void AZippyCharacter::MoveRight(float Value)
 {
 	if ( (Controller != nullptr) && (Value != 0.0f) )
 	{
+		// find out which way is right
 		const FRotator Rotation = Controller->GetControlRotation();
 		const FRotator YawRotation(0, Rotation.Yaw, 0);
+	
+		// get right vector 
 		const FVector Direction = FRotationMatrix(YawRotation).GetUnitAxis(EAxis::Y);
+		// add movement in that direction
 		AddMovementInput(Direction, Value);
 	}
 }
